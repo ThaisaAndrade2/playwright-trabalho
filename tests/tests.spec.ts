820525ad2640cbfa7684aba4acef2453ea0cb9ec
ThaisaAndrade2 <thaisasantosandrade7@gmail.com>
2024-05-09 19:54:00 -0300

    Adding new validations

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -58,10 +58,60 @@ test ('Cenario 3: Fazendo uma conta', async ({ page }) => {
     await page.getByText('Welcome, Thaisa Santos! Change').textContent;
 })
 
-
 test ('Cenário 4: Adicionando alguns produtos ao carrinho de compras', async({ page }) => {
+    //inicio
     await page.goto('https://magento.softwaretestingboard.com/');
-    await page.getByText('Shop New Yoga').click();
-    const text = await page.getByText('Shopping Options').textContent();
-    console.log(text)
+    await page.locator('text=Shop New Yoga').click();
+    await page.locator('text=Shopping Options').textContent();
+
+    // Primeiro produto selecionado
+    await page.locator('text=Echo Fit Compression Short').click();
+    await page.locator('text=Echo Fit Compression Short').textContent;
+    await page.locator('text=Size').textContent;
+    await page.locator('div[id="option-label-size-143-item-171"]').click();
+    await page.locator('div[id*="option-label-color-93-item-50"]').click();
+    await page.getByText('Qty').textContent;
+    await page.locator('input[id*="qty"]').fill('10');
+    await page.locator('text=Add to Cart').click();
+    await page.locator('text=Adding...').textContent;
+    await page.locator('text=Added').textContent;
+    await page.locator('text=You added Echo Fit Compression Short to your shopping cart.').textContent;
+
+    // Segundo produto selecionado
+    await page.locator('text=Prima Compete Bra Top').click();
+    await page.locator('text=Prima Compete Bra Top').textContent;
+    await page.locator('text=Size').textContent;
+    await page.locator('div[id="option-label-size-143-item-168"]').click();
+    await page.locator('div[id*="option-label-color-93-item-50"]').click();
+    await page.locator('text=Qty').textContent;
+    await page.locator('input[id*="qty"]').click();
+    await page.locator('input[id*="qty"]').fill('3');
+    await page.locator('text=Add to Cart').click();
+    await page.locator('text=Adding...').textContent;
+    await page.locator('text=Added').textContent;
+    await page.locator('text=You added Prima Compete Bra Top to your shopping cart.').textContent;
+
+    //Terceiro produto selecionado
+    await page.locator('text=Erika Running Short').click();
+    await page.locator('text=Erika Running Short').textContent;
+    await page.locator('text=Size').textContent;
+    await page.locator('div[id="option-label-size-143-item-172"]').click();
+    await page.locator('div[id*="option-label-color-93-item-53"]').click();
+    await page.locator('text=Qty').textContent;
+    await page.locator('input[id*="qty"]').click();
+    await page.locator('input[id*="qty"]').fill('1');
+    await page.locator('text=Add to Cart').click();
+    await page.locator('text=Adding...').textContent;
+    await page.locator('text=Added').textContent;
+    await page.locator('text=You added Erika Running Short to your shopping cart.').textContent;
 })
+
+test('Cenário 5: Buscando por um produto na lupa', async ({ page }) => {
+    await page.goto('https://magento.softwaretestingboard.com/');
+    await page.waitForSelector('text=Get fit and look fab in new seasonal styles');
+    await page.fill('input[placeholder="Search entire store here..."]', 'Montana Wind Jacket');
+    await page.press('input[placeholder="Search entire store here..."]', 'Enter');
+    await page.getByText('Related search terms').textContent;
+});
+
+
