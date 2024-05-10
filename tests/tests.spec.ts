0da85b867eb0e67e3c1ab3df902f49d9f2d56cdf
ThaisaAndrade2 <thaisasantosandrade7@gmail.com>
2024-05-09 21:57:54 -0300

    Atualizando os cenarios corrigindo erros

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,23 +1,5 @@
 import { test } from "playwright/test";
 
-// Função para gerar um e-mail aleatório
-function generateRandomEmail() {
-    const randomString = Math.random().toString(36).substring(7);
-    return `user_${randomString}@example.com`;
-}
-
-// Função para gerar uma senha aleatória
-function generateRandomPassword(length = 8) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const symbols = '!@#*'
-    let password = '';
-    for (let i = 0; i < length; i++) {
-        password += symbols + characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return password;
-}
-
-
 test ('Cenário 1: Abrindo o site e validando as informações do menu bar', async({ page }) => {
     await page.goto('https://magento.softwaretestingboard.com/');
     await page.getByText("What's New").textContent();
@@ -30,33 +12,52 @@ test ('Cenário 1: Abrindo o site e validando as informações do menu bar', asy
 })
 
 
-test ('Cenario 2: Criando uma conta', async ({ page }) => {
-    await page.goto('https://magento.softwaretestingboard.com/');   
-    const createAccount = page.getByText('Create an Account').first()
-    createAccount.click();
-    // Preencher o formulário de registro com dados aleatórios
-    await page.getByTestId('firstname').fill('Thaisa');
-    await page.getByTestId('lastname').fill('Santos');
-    await page.getByTestId('email_address').fill(generateRandomEmail());
+test('Cenario 2: Criando uma conta', async ({ page }) => {
+    // Função para gerar um e-mail aleatório
+    function generateRandomEmail() {
+        const randomString = Math.random().toString(36).substring(7);
+        return `user_${randomString}@example.com`;
+    }
+
+    // Função para gerar uma senha aleatória
+    function generateRandomPassword(length = 8) {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const symbols = '!@#*'
+        let password = '';
+        for (let i = 0; i < length; i++) {
+            password += symbols + characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        return password;
+    }
+
+    await page.goto('https://magento.softwaretestingboard.com/');
+    await page.waitForSelector('text=Create an Account');
+    await page.click('text=Create an Account');
+    // Esperar que os campos de entrada estejam disponíveis
+    await page.waitForSelector('[id=firstname]');
+    await page.fill('[id=firstname]', 'Thaisa');
+    await page.fill('[id=lastname]', 'Santos');
+    await page.fill('[id=email_address]', generateRandomEmail());
     const randomPassword = generateRandomPassword();
-    await page.getByTestId('password').fill(randomPassword);
-    await page.getByTestId('password-confirmation').fill(randomPassword);
-    await page.locator('.action.submit.primary').click();
-    await page.getByText('Thank you for registering with Main Website Store.').textContent(); 
+    await page.fill('[id=password]', randomPassword);
+    await page.fill('[id=password-confirmation]', randomPassword);
+    await page.click('.action.submit.primary');
+    await page.waitForSelector('text=Thank you for registering with Main Website Store.');
 })
 
 
 
-test ('Cenario 3: Fazendo uma conta', async ({ page }) => {
+test('Cenario 3: Login copm dados corretos', async ({ page }) => {
     await page.goto('https://magento.softwaretestingboard.com/');
-    const elemento1= await page.locator('li.authorization-link').first()
-    elemento1.click();
-    page.getByText('Registered Customers').textContent();
-    await page.getByTestId('email').fill('thaisa.test@gmail.com');
-    await page.getByTestId('pass').fill('senha12@');
+    const elemento1 = await page.locator('li.authorization-link').first();
+    await elemento1.click();
+    await page.getByText('Registered Customers').textContent();
+    await page.locator('input[id=email]').fill('thaisa.test@gmail.com');
+    await page.locator('input[id=pass]').fill('senha12@');
     await page.locator('.action.login.primary').click();
     await page.getByText('Welcome, Thaisa Santos! Change').textContent;
-})
+});
+
 
 test ('Cenário 4: Adicionando alguns produtos ao carrinho de compras', async({ page }) => {
     //inicio
@@ -70,7 +71,6 @@ test ('Cenário 4: Adicionando alguns produtos ao carrinho de compras', async({ 
     await page.locator('text=Size').textContent;
     await page.locator('div[id="option-label-size-143-item-171"]').click();
     await page.locator('div[id*="option-label-color-93-item-50"]').click();
-    await page.getByText('Qty').textContent;
     await page.locator('input[id*="qty"]').fill('10');
     await page.locator('text=Add to Cart').click();
     await page.locator('text=Adding...').textContent;
@@ -99,7 +99,7 @@ test ('Cenário 4: Adicionando alguns produtos ao carrinho de compras', async({ 
     await page.locator('div[id*="option-label-color-93-item-53"]').click();
     await page.locator('text=Qty').textContent;
     await page.locator('input[id*="qty"]').click();
-    await page.locator('input[id*="qty"]').fill('1');
+    await page.locator('input[id*="qty"]').fill('2');
     await page.locator('text=Add to Cart').click();
     await page.locator('text=Adding...').textContent;
     await page.locator('text=Added').textContent;
@@ -108,10 +108,28 @@ test ('Cenário 4: Adicionando alguns produtos ao carrinho de compras', async({ 
 
 test('Cenário 5: Buscando por um produto na lupa', async ({ page }) => {
     await page.goto('https://magento.softwaretestingboard.com/');
-    await page.waitForSelector('text=Get fit and look fab in new seasonal styles');
+    await page.getByText('Shop New Yoga').textContent;   
     await page.fill('input[placeholder="Search entire store here..."]', 'Montana Wind Jacket');
     await page.press('input[placeholder="Search entire store here..."]', 'Enter');
-    await page.getByText('Related search terms').textContent;
+    await page.waitForSelector('text=Related search terms');
+})
+
+test('Cenário 6: Buscando por um produto inexistente', async ({ page }) => {
+    await page.goto('https://magento.softwaretestingboard.com/');
+    await page.getByText('Shop New Yoga').textContent;   
+    await page.fill('input[placeholder="Search entire store here..."]', 'Testes');
+    await page.press('input[placeholder="Search entire store here..."]', 'Enter');
+    await page.waitForSelector('text=Your search returned no results.');
 });
 
 
+test('Cenario 7: Login com dados inexistentes', async ({ page }) => {
+    await page.goto('https://magento.softwaretestingboard.com/');
+    const elemento1 = await page.locator('li.authorization-link').first();
+    await elemento1.click();
+    await page.getByText('Registered Customers').textContent();
+    await page.locator('input[id=email]').fill('thaisa99.test@gmail.com');
+    await page.locator('input[id=pass]').fill('senha99@');
+    await page.locator('.action.login.primary').click();
+    await page.getByText('The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.').textContent;
+});
